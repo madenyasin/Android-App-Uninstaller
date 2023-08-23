0e8487f4d838ef90ea2c1079a935c990eaac1a4e
Yasin Maden <maden.yasin@hotmail.com>
2023-08-23 17:13:11 +0300

    app_list metodu daha iyi bir şekilde yeniden yazıldı

--- project.py
+++ project.py
@@ -12,26 +12,29 @@ package_names = []
 serial_number = ""
 log_file = "log.txt"
 
+
 def main():
     global root
     set_colors()
 
-    messagebox.showinfo("Welcome", "First you have to choose a device.")
-    
-    list_devices()  
+    list_devices()
     root.mainloop()
-    
+
+
 # detect user's OS
 def get_OS():
     return platform.system().lower()
 
+
 def get_adb_folder():
     return os.path.join(os.getcwd(), ("adb\\" + get_OS() + "\\platform-tools"))
 
+
 def run_command(directory, command):
-    command = f"cd {directory} & {command}" 
+    command = f"cd {directory} & {command}"
     p = os.popen(command)
     return p.read()
+
 
 # get search key
 def Scankey(event):
@@ -66,25 +69,39 @@ def Update(data):
 
 
 def list_devices():
-    if run_command(get_adb_folder(), "adb devices").splitlines()[1] == "":
-        design.device_chose_cmb.set("No device connected!")
-    else:
-        response = run_command(get_adb_folder(), "adb devices")
-        devices = response.replace("\tdevice", "").splitlines()
-        for device in devices:
-            if "\tunauthorized" in device:
-                device = device.replace('\tunauthorized', '')
-                print(f"{device} Nolu cihazın USB hata ayıklama yetkisi yok! Lütfen yetki verin.")
-        
-        devices = response.splitlines()[1:-1]
-        device_numbers = []
-        for item in devices:
-            if "\tunauthorized" in item:
-                device_numbers.append(item.replace("\tunauthorized", ""))
-            elif "\tdevice" in item:
-                device_numbers.append(item.replace("\tdevice", ""))
-        design.device_chose_cmb['values'] = tuple(device_numbers)
+    device_list = run_command(get_adb_folder(), "adb devices").splitlines()[1:-1]
+
+    # USB debugging close || No phone connection
+    if not device_list:
+        messagebox.showwarning(
+            "Device not detected",
+            "1) Connect your phone.\n"
+            + "2) Enable USB debugging in your phone's settings.",
+        )
     
+    for item in device_list:
+        # USB debugging is not allowed.
+        if "unauthorized" in item:
+            messagebox.showerror(
+                "USB debugging is not allowed",
+                "Please allow USB debugging in your device's settings.\n\n"
+                + "Device serial number: "
+                + item.replace("\tunauthorized", ""),
+            )
+            device_list.remove(item)
+            continue
+        # other negative results were excluded from the list.
+        elif "device" not in item:
+            device_list.remove(item)
+
+    # Extraction of serial number.
+    for i in range(len(device_list)):
+        device_list[i] = device_list[i].replace("\tdevice", "")
+        
+    print(device_list)
+    design.device_chose_cmb['values'] = device_list
+    
+
 design.refresh_devices_btn.config(command=list_devices)
 
 
@@ -93,7 +110,7 @@ def list_apps():
     global serial_number
     global package_names
 
-    design.app_list_listbox.delete(0, tk.END) #listbox clear
+    design.app_list_listbox.delete(0, tk.END)  # listbox clear
     design.search_box.delete(0, tk.END)  # Clear existing text
 
     if design.device_chose_cmb.get() in ["No device connected!", "Select Your Device"]:
@@ -108,31 +125,41 @@ def list_apps():
             package_names.append(app.replace("package:", ""))
             design.app_list_listbox.insert("end", app.replace("package:", ""))
 
-    
+
 design.refresh_app_list_btn.config(command=list_apps)
 
 
 def modified_cmb(event):
     global serial_number
-    if design.device_chose_cmb.get() not in ["No device connected!", "Select Your Device"]:
+    if design.device_chose_cmb.get() not in [
+        "No device connected!",
+        "Select Your Device",
+    ]:
         serial_number = design.device_chose_cmb.get()
-
 
     list_apps()
 
-design.device_chose_cmb.bind('<<ComboboxSelected>>', modified_cmb)
+
+design.device_chose_cmb.bind("<<ComboboxSelected>>", modified_cmb)
+
 
 def uninstall_app():
     global package_names
     global serial_number
     global log_file
     if not package_names:
-        messagebox.showwarning("No application selected", "Please select the application you want to uninstall.")
+        messagebox.showwarning(
+            "No application selected",
+            "Please select the application you want to uninstall.",
+        )
     else:
-
-        package_name = design.app_list_listbox.get(design.app_list_listbox.curselection())   
+        package_name = design.app_list_listbox.get(
+            design.app_list_listbox.curselection()
+        )
         if package_name:
-            command = f"adb -s {serial_number} shell pm uninstall --user 0 {package_name}"
+            command = (
+                f"adb -s {serial_number} shell pm uninstall --user 0 {package_name}"
+            )
             response = run_command(get_adb_folder(), command)
 
             if "success" in response.lower():
@@ -145,9 +172,11 @@ def uninstall_app():
             else:
                 print(response)
                 with open(log_file, "a") as file:
-                    file.writelines(f"device: {serial_number} | state: {response} -> {package_name} | date of removal: {datetime.datetime.now()}\n")
+                    file.writelines(
+                        f"device: {serial_number} | state: {response} -> {package_name} | date of removal: {datetime.datetime.now()}\n"
+                    )
 
-    
+
 design.uninstall_app_btn.config(command=uninstall_app)
 
 
@@ -171,7 +200,7 @@ def set_colors():
     design.uninstall_app_btn.config(bg=color_btn)
     design.refresh_app_list_btn.config(bg=color_btn)
 
-    text_color= "#000000"
+    text_color = "#000000"
     font_name = ("Comic Sans MS", 12, "normal")
 
     design.device_chose_cmb.config(foreground=text_color, font=font_name)
@@ -181,8 +210,9 @@ def set_colors():
     design.refresh_app_list_btn.config(foreground=text_color, font=font_name)
 
     design.search_box.config(foreground="#FFFFFF", font=font_name)
-    design.app_list_listbox.config(foreground="#FFFFFF", font=("Segoe UI", 13, "normal"))
-
+    design.app_list_listbox.config(
+        foreground="#FFFFFF", font=("Segoe UI", 13, "normal")
+    )
 
 
 if __name__ == "__main__":
